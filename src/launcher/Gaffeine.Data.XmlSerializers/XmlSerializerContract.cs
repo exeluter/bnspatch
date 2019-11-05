edd08033710970ea917deb2195f52638735bef3a
zeffy <zeffy@users.noreply.github.com>
2019-11-05 02:22:33 -0800

    Make Gaffeine.Data.XmlSerializers public

---
+++ src/launcher/Gaffeine.Data.XmlSerializers/XmlSerializerContract.cs
@@ -0,0 +1,132 @@
+﻿using Gaffeine.Data;
+using Gaffeine.Data.Models;
+using Microsoft.ApplicationInsights.Extensibility;
+using MonoMod.RuntimeDetour;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Linq;
+using NCLauncherW.Views;
+using System.Windows;
+using MoreLinq.Extensions;
+using System.Security.Principal;
+using NCLog;
+
+namespace Microsoft.Xml.Serialization.GeneratedAssembly
+{
+  public partial class XmlSerializerContract : global::System.Xml.Serialization.XmlSerializerImplementation
+  {
+    [DllImport("kernel32.dll")]
+    static extern void OutputDebugString(string lpOutputString);
+
+    static XmlSerializerContract()
+    {
+      AppDomain.CurrentDomain.AssemblyLoad += OnAssemblyLoad;
+      AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+      RegisterRuntimeDetours();
+    }
+
+    static void RegisterRuntimeDetours()
+    {
+      /* Disable Application Insights telemetry
+         Important note: Exactly one event will be tracked before this hook is able to disable it,
+         which is just "NCLauncher2 Startup". To completely disable telemetry you need to overwrite
+         ApplicationInsights.config with this:
+     
+         <?xml version="1.0" encoding="utf-8"?>
+         <ApplicationInsights xmlns="http://schemas.microsoft.com/ApplicationInsights/2013/Settings">
+           <DisableTelemetry>true</DisableTelemetry>
+         </ApplicationInsights>
+      */
+      _ = new Hook(
+        typeof(TelemetryConfiguration)
+          .GetProperty(nameof(TelemetryConfiguration.DisableTelemetry))
+          .GetGetMethod(),
+        new Func<Func<TelemetryConfiguration, bool>, TelemetryConfiguration, bool>((fn, @this) => true));
+
+      /* Fixes issue where if you click on the password field without filling in the email address field,
+         it will be focused instead of the password field. */
+      _ = new Hook(
+        typeof(SignInWindow).GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
+                                       .FirstOrDefault(x => x.GetParameters()
+                                                             .Select(y => y.ParameterType)
+                                                             .SequenceEqual(new[] { typeof(UIElement), typeof(bool) })),
+        new Action<Action<SignInWindow, UIElement, bool>, SignInWindow, UIElement, bool>((fn, @this, A_1, A_2) => { }));
+
+      // Forward command line arguments that aren't for NC Launcher 2 to the game client.
+      _ = new Hook(
+        typeof(GameInfo)
+          .GetProperty(nameof(GameInfo.ExeArgument))
+          .GetGetMethod(),
+        new Func<Func<GameInfo, string>, GameInfo, string>((fn, @this) =>
+          string.Join(" ", Environment.GetCommandLineArgs()
+                                      .Skip(1)
+                                      .Where(x => !string.IsNullOrEmpty(x)
+                                                  && !x.StartsWith("nc-launcher2://")
+                                                  && !x.StartsWith("nc-launcher2beta://")
+                                                  && !(x.StartsWith("/") && x.IndexOf(':') > -1))
+                                      .Prepend(fn(@this)))));
+
+      /* Fixes bug where unnecessary localization files are downloaded. The funny thing is, it seems
+         like they intended these files to be skipped, but the code is bugged from the start and it
+         has never been fixed. */
+      _ = new Hook(
+        typeof(LanguagePackageFiles)
+          .GetMethod(nameof(LanguagePackageFiles.Exists), new[] { typeof(string) }),
+        new Func<Func<LanguagePackageFiles, string, bool>, LanguagePackageFiles, string, bool>((fn, @this, fileName) =>
+          !string.IsNullOrEmpty(fileName)
+          && @this.Exists((LanguagePackageFile x) => fileName.IndexOf(x.FileName, StringComparison.InvariantCultureIgnoreCase) > -1)));
+
+      /* Send unencrypted log messages to OutputDebugString */
+      _ = new Hook(
+        typeof(Logger)
+          .GetMethod(nameof(Logger.Debug)), 
+        new Action<Action<Logger, string>, Logger, string>((fn, @this, message) => {
+          OutputDebugString("DEBUG - " + message);
+          fn(@this, message); }));
+
+      _ = new Hook(
+        typeof(Logger)
+          .GetMethod(nameof(Logger.Info)),
+        new Action<Action<Logger, string>, Logger, string>((fn, @this, message) => {
+          OutputDebugString("INFO - " + message);
+          fn(@this, message);
+        }));
+
+      _ = new Hook(
+        typeof(Logger)
+          .GetMethod(nameof(Logger.Error), new[] { typeof(string) }),
+        new Action<Action<Logger, string>, Logger, string>((fn, @this, message) => {
+          OutputDebugString("ERROR - " + message);
+          fn(@this, message);
+        }));
+    }
+
+    private static void OnAssemblyLoad(object sender, AssemblyLoadEventArgs args) =>
+      OutputDebugString("Assembly loaded: " + args.LoadedAssembly.FullName);
+
+    private static Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+    {
+      var assemblyName = new AssemblyName(args.Name);
+      var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(assemblyName.Name + ".dll.deflate");
+      if ( stream != null ) {
+        try {
+          using ( var binaryReader = new BinaryReader(stream) )
+          using ( var deflateStream = new DeflateStream(stream, CompressionMode.Decompress) )
+          using ( var memoryStream = new MemoryStream(binaryReader.ReadInt32()) ) {
+            deflateStream.CopyTo(memoryStream);
+            var assembly = Assembly.Load(memoryStream.GetBuffer());
+            if ( assembly != null )
+              return assembly;
+          }
+        } finally {
+          stream.Dispose();
+        }
+      }
+      OutputDebugString("Assembly not resolved: " + args.Name);
+      return null;
+    }
+  }
+}
