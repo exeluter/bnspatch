1f17f0bcc12968e1b1c4ee6eed2d9beb46e9a56b
zeffy <zeffy@users.noreply.github.com>
2019-11-09 03:18:24 -0800

    Don't overwrite game shortcut if it already exists

---
+++ src/Gaffeine.Data.XmlSerializers/IntrospectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using MonoMod.Utils;
+
+public static class IntrospectionExtensions
+{
+  static readonly FastReflectionDelegate GetSimpleNameDelegate;
+
+  static IntrospectionExtensions()
+  {
+    GetSimpleNameDelegate = Type.GetType("System.Reflection.RuntimeAssembly")?
+                                .GetMethod("GetSimpleName", BindingFlags.Instance | BindingFlags.NonPublic)?
+                                .GetFastDelegate();
+  }
+
+  public static string GetSimpleName(this Assembly @this) =>
+    GetSimpleNameDelegate?.Invoke(@this) as string;
+}
--- src/Gaffeine.Data.XmlSerializers/XmlSerializerContract.cs
+++ src/Gaffeine.Data.XmlSerializers/XmlSerializerContract.cs
@@ -1,26 +1,22 @@
 ﻿using Gaffeine.Data;
 using Gaffeine.Data.Models;
-using Mono.Cecil;
 using MonoMod.RuntimeDetour;
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Windows;
 using System.Xml.Serialization;
-using static MoreLinq.Extensions.PrependExtension;
+using MoreLinq;
+using Microsoft.CSharp.RuntimeBinder;
 
 namespace Microsoft.Xml.Serialization.GeneratedAssembly
 {
   public class XmlSerializerContract : XmlSerializerImplementation
   {
-    static readonly string EntryAssemblySimpleName;
-    static readonly Dictionary<string, string> AppArgs;
 
     static XmlSerializerContract()
     {
@@ -28,21 +24,7 @@ namespace Microsoft.Xml.Serialization.GeneratedAssembly
         NativeMethods.OutputDebugString("Assembly loaded: " + args.LoadedAssembly.FullName);
       AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
 
-      EntryAssemblySimpleName = Assembly.GetEntryAssembly().GetSimpleName();
-      string typeName = null;
-      if ( EntryAssemblySimpleName == "NCLauncher2" )
-        typeName = "NCLauncherW.AppArgs, NCLauncher2";
-      else if ( EntryAssemblySimpleName == "SelfUpdater" )
-        typeName = "SelfUpdater.Settings.AppArgs, SelfUpdater";
-
-      if ( typeName != null ) {
-        AppArgs = Type.GetType(typeName)?
-                      .GetProperty("Instance", BindingFlags.Static | BindingFlags.Public)?
-                      .GetValue(null)?
-                      .GetPropertyValues<string>();
-        if ( AppArgs != null )
-          RegisterRuntimeDetours();
-      }
+      RegisterRuntimeDetours();
     }
 
     public override XmlSerializationReader Reader => null;
@@ -55,97 +37,67 @@ namespace Microsoft.Xml.Serialization.GeneratedAssembly
 
     private static void RegisterRuntimeDetours()
     {
-      /* Disable Application Insights telemetry
-       * Important note: Exactly one event will be tracked before this hook is able to disable it,
-       * which is just "NCLauncher2 Startup". To completely disable telemetry you need to overwrite
-       * ApplicationInsights.config with this:
-
-         <?xml version="1.0" encoding="utf-8"?>
-         <ApplicationInsights xmlns="http://schemas.microsoft.com/ApplicationInsights/2013/Settings">
-           <DisableTelemetry>true</DisableTelemetry>
-         </ApplicationInsights>
-      */
+      // Disable Application Insights telemetry
+      // Important note: Exactly one event will be tracked before this hook is able to disable it,
+      // which is just "NCLauncher2 Startup". To completely disable telemetry you need to overwrite
+      // ApplicationInsights.config with the one in the Resources folder. 
       _ = new Hook(Type.GetType("Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration, Microsoft.ApplicationInsights")
                        .GetProperty("DisableTelemetry")
                        .GetGetMethod(),
-        new Func<Func<object, bool>, object, bool>((fn, @this) => true));
+        new Func<Func<object, bool>, object, bool>((_, @this) => true));
 
-      /* Forward command line arguments that probably aren't for NC Launcher 2, to the game specified by 
-       * the GameID launcher argument.
-       */
+      // Forward command line arguments that probably aren't for NC Launcher 2, to the game specified by 
+      // the GameID launcher argument. 
       _ = new Hook(typeof(GameInfo).GetProperty(nameof(GameInfo.ExeArgument))
                                    .GetGetMethod(),
-        new Func<Func<GameInfo, string>, GameInfo, string>((fn, @this) =>
-            AppArgs.TryGetValue("GameId", out string value) && string.Equals(@this.GameId, value, StringComparison.OrdinalIgnoreCase)
-              ? string.Join(" ", Environment.GetCommandLineArgs()
-                                            .Skip(1)
-                                            .Where(x => !string.IsNullOrEmpty(x)
-                                                     && !x.StartsWith("nc-launcher2://")
-                                                     && !x.StartsWith("nc-launcher2beta://")
-                                                     && !(x.StartsWith("/") && x.Contains(":")))
-                                            .Prepend(fn(@this)))
-              : fn(@this)));
+        new Func<Func<GameInfo, string>, GameInfo, string>((fn, @this) => {
+          try {
+            if ( @this.GameId.Equals(AppArgs.Instance.GameId, StringComparison.OrdinalIgnoreCase) )
+              return string.Join(" ", Environment.GetCommandLineArgs()
+                                                 .Skip(1)
+                                                 .Where(x => !string.IsNullOrEmpty(x)
+                                                             && !x.StartsWith("nc-launcher2://")
+                                                             && !x.StartsWith("nc-launcher2beta://")
+                                                             && !(x.StartsWith("/") && x.Contains(":")))
+                                                 .Prepend(fn(@this)));
+          } catch ( RuntimeBinderException ) {
+          }
+          return fn(@this);
+        }));
 
-      /* Fixes bug where unnecessary localization files are downloaded. The funny thing is, it seems
-       * like they intended for these files to be skipped, but the code was bugged from the start and it
-       * has never been fixed. 
-       */
+      // Don't overwrite the game shortcut if it already exists.
+      _ = new Hook(Type.GetType("Gaffeine.Controls.Helpers.ShortcutHelper, Gaffeine.Controls")
+                       .GetMethod("MakeGameShortcut"),
+        new Func<Func<Game, bool>, Game, bool>((fn, game) =>
+          File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), game.GameName + ".lnk"))
+          || fn(game)));
+
+      // Fixes bug where unnecessary localization files are downloaded. The funny thing is, it seems
+      // like they intended for these files to be skipped, but the code was bugged from the start and it
+      // has never been fixed.
       _ = new Hook(typeof(LanguagePackageFiles).GetMethod(nameof(LanguagePackageFiles.Exists), new[] { typeof(string) }),
-        new Func<Func<LanguagePackageFiles, string, bool>, LanguagePackageFiles, string, bool>((fn, @this, fileName) =>
+        new Func<Func<LanguagePackageFiles, string, bool>, LanguagePackageFiles, string, bool>((_, @this, fileName) =>
           !string.IsNullOrEmpty(fileName) && @this.Exists(x => fileName.Contains(x.FileName, StringComparison.OrdinalIgnoreCase))));
 
-      /* Send unencrypted log messages to OutputDebugString 
-       */
-      _ = new Hook(Type.GetType("NCLog.Logger, NCLog")
-                       .GetMethod("Debug"),
-        new Action<Action<object, string>, object, string>((fn, @this, message) => {
-          NativeMethods.OutputDebugString("DEBUG - " + message);
-          fn(@this, message);
-        }));
+      // Turn off encryption of log messages
+      Type.GetType("NCLog.Logger, NCLog")
+          .GetProperty("UseEncryption")
+          .SetValue(null, false);
 
-      _ = new Hook(Type.GetType("NCLog.Logger, NCLog")
-                       .GetMethod("Info"),
-        new Action<Action<object, string>, object, string>((fn, @this, message) => {
-          NativeMethods.OutputDebugString("INFO - " + message);
-          fn(@this, message);
-        }));
-
-      _ = new Hook(Type.GetType("NCLog.Logger, NCLog")
-                       .GetMethod("Error", new[] { typeof(string) }),
-        new Action<Action<object, string>, object, string>((fn, @this, message) => {
-          NativeMethods.OutputDebugString("ERROR - " + message);
-          fn(@this, message);
-        }));
-
-      if ( EntryAssemblySimpleName == "NCLauncher2" ) {
-        /* Fixes issue where if you click on the password field without filling in the email address field,
-         * it will be focused instead of the password field. 
-         */
+      if ( Assembly.GetEntryAssembly().GetSimpleName() == "NCLauncher2" ) {
+        // Fixes annoying behavior when you click on the password field without filling in the email address field,
+        // it will be focused instead of the password field (and vice versa). 
         var from = Type.GetType("NCLauncherW.Views.SignInWindow, NCLauncher2")
                        .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
                        .FirstOrDefault(x => x.ReturnType == typeof(void)
-                                         && x.GetParameters()
-                                             .Select(y => y.ParameterType)
-                                             .SequenceEqual(new[] { typeof(UIElement), typeof(bool) }));
+                                            && x.GetParameters()
+                                                .Select(y => y.ParameterType)
+                                                .SequenceEqual(new[] { typeof(UIElement), typeof(bool) }));
         if ( from != null ) {
           _ = new Hook(from,
-            new Action<Action<object, UIElement, bool>, object, UIElement, bool>((fn, @this, A_1, A_2) => { }));
+            new Action<Action<object, UIElement, bool>, object, UIElement, bool>((_, @this, arg1, arg2) => { }));
         };
-      } else if ( EntryAssemblySimpleName == "SelfUpdater" ) {
-
       }
-    }
-
-    private static string GenerateAssemblyId(AssemblyDefinition assemblyDef)
-    {
-      var sb = new StringBuilder();
-      foreach ( var mvid in assemblyDef.Modules
-                                       .Select(x => x.Mvid.ToString())
-                                       .OrderBy(x => x) ) {
-        sb.Append(mvid);
-        sb.Append(',');
-      }
-      return sb.ToString();
     }
 
     private static Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
@@ -165,6 +117,30 @@ namespace Microsoft.Xml.Serialization.GeneratedAssembly
       }
       NativeMethods.OutputDebugString("Assembly not resolved: " + args.Name);
       return null;
+    }
+    private class AppArgs
+    {
+      public static readonly dynamic Instance;
+
+      static AppArgs()
+      {
+        // Super hack-ish, but whatever.
+        string typeName = null;
+        switch ( Assembly.GetEntryAssembly().GetSimpleName() ) {
+        case "NCLauncher2":
+          typeName = "NCLauncherW.AppArgs, NCLauncher2";
+          break;
+        case "SelfUpdater":
+          typeName = "SelfUpdater.Settings.AppArgs, SelfUpdater";
+          break;
+        }
+        if ( typeName != null ) {
+          Instance = Type.GetType(typeName)?
+                         .GetProperty("Instance")?
+                         .GetGetMethod()
+                         .Invoke(null, null);
+        }
+      }
     }
 
     private static class NativeMethods
