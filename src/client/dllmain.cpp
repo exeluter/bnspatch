1b66f8dcfa0bfdbdf661a1d82a8c6007473555d1
zeffy <zeffy@users.noreply.github.com>
2020-02-23 14:11:58 -0800

    upd

--- src/client/dllmain.cpp
+++ src/client/dllmain.cpp
@@ -7,20 +7,20 @@
 
 struct hook_t
 {
-  const char *name;
-  void **ptr;
-  void *detour;
+  PCSTR ProcedureName;
+  PVOID *ProcedureAddress;
+  PVOID DetourAddress;
 };
 
 template <size_t Size>
-void AttachMultipleDetours(
+inline void AttachMultipleDetours(
   const wchar_t *module_name,
   const std::array<hook_t, Size> &detours)
 {
   if ( const auto module = pe::get_module(module_name) ) {
     for ( const auto &det : detours ) {
-      if ( *det.ptr = module->find_function(det.name) )
-        DetourAttach(det.ptr, det.detour);
+      if ( *det.ProcedureAddress = module->find_function(det.ProcedureName) )
+        DetourAttach(det.ProcedureAddress, det.DetourAddress);
     }
   }
 }
@@ -31,8 +31,6 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, LPVOID lpvReserved)
     case DLL_PROCESS_ATTACH: {
       if ( pe::get_module()->base_name() == xorstr_(L"Client.exe") ) {
         NtCurrentPeb()->BeingDebugged = FALSE;
-
-        // Register Dll Notification
         if ( const auto module = pe::get_module(xorstr_(L"ntdll.dll")) ) {
           if ( const auto pfn = reinterpret_cast<decltype(&LdrRegisterDllNotification)>(
             module->find_function(xorstr_("LdrRegisterDllNotification"))) ) {
@@ -40,21 +38,20 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, LPVOID lpvReserved)
           }
         }
 
-        // Attach Detours
         DetourTransactionBegin();
         DetourUpdateThread(NtCurrentThread());
         AttachMultipleDetours(xorstr_(L"ntdll.dll"),
           std::array {
-            hook_t { xorstr_("LdrLoadDll"), &(void *&)g_pfnLdrLoadDll, (void *)&LdrLoadDll_hook },
-            hook_t { xorstr_("NtCreateFile"), &(void *&)g_pfnNtCreateFile, (void *)&NtCreateFile_hook },
-            hook_t { xorstr_("NtCreateMutant"), &(void *&)g_pfnNtCreateMutant, (void *)&NtCreateMutant_hook },
-            hook_t { xorstr_("NtProtectVirtualMemory"), &(void *&)g_pfnNtProtectVirtualMemory, (void *)&NtProtectVirtualMemory_hook },
-            hook_t { xorstr_("NtQueryInformationProcess"), &(void *&)g_pfnNtQueryInformationProcess, (void *)&NtQueryInformationProcess_hook },
-            hook_t { xorstr_("NtQuerySystemInformation"), &(void *&)g_pfnNtQuerySystemInformation, (void *)&NtQuerySystemInformation_hook },
+            hook_t { xorstr_("LdrLoadDll"), &(PVOID &)g_pfnLdrLoadDll, &LdrLoadDll_hook },
+            hook_t { xorstr_("NtCreateFile"), &(PVOID &)g_pfnNtCreateFile, &NtCreateFile_hook },
+            hook_t { xorstr_("NtCreateMutant"), &(PVOID &)g_pfnNtCreateMutant, &NtCreateMutant_hook },
+            hook_t { xorstr_("NtProtectVirtualMemory"), &(PVOID &)g_pfnNtProtectVirtualMemory, &NtProtectVirtualMemory_hook },
+            hook_t { xorstr_("NtQueryInformationProcess"), &(PVOID &)g_pfnNtQueryInformationProcess, &NtQueryInformationProcess_hook },
+            hook_t { xorstr_("NtQuerySystemInformation"), &(PVOID &)g_pfnNtQuerySystemInformation, &NtQuerySystemInformation_hook },
           });
         AttachMultipleDetours(xorstr_(L"user32.dll"),
           std::array {
-            hook_t { xorstr_("FindWindowA"), &(PVOID &)g_pfnFindWindowA, (PVOID)&FindWindowA_hook }
+            hook_t { xorstr_("FindWindowA"), &(PVOID &)g_pfnFindWindowA, &FindWindowA_hook }
           });
         DetourTransactionCommit();
         break;
@@ -65,23 +62,23 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, LPVOID lpvReserved)
 }
 
 ExternC const PfnDliHook __pfnDliNotifyHook2 = [](unsigned dliNotify, PDelayLoadInfo pdli) -> FARPROC {
-  std::array<WCHAR, MAX_PATH> buffer;
+  std::array<WCHAR, MAX_PATH> Buffer;
 
   switch ( dliNotify ) {
     case dliStartProcessing:
       break;
-    case dliNotePreLoadLibrary: {
+    case dliNotePreLoadLibrary:
       if ( !_stricmp(pdli->szDll, pe::get_instance_module()->export_directory()->name())
-        && GetSystemDirectoryW(buffer.data(), SafeInt(buffer.size())) ) {
+        && GetSystemDirectoryW(Buffer.data(), SafeInt(Buffer.size())) ) {
 
-        auto path = fs::path(buffer.data());
-        path /= pdli->szDll;
+        auto Path = fs::path(Buffer.data());
+        Path /= pdli->szDll;
+        auto Module = reinterpret_cast<pe::module *>(
+          LoadLibraryExW(Path.c_str(), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH));
 
-        return reinterpret_cast<FARPROC>(
-          LoadLibraryExW(path.c_str(), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH));
+        return reinterpret_cast<FARPROC>(Module);
       }
       break;
-    }
     case dliNotePreGetProcAddress:
     case dliFailLoadLib:
     case dliFailGetProc:
