81e46253535eb58a631999573c12894f1b1bb24f
pilao <zeffy@users.noreply.github.com>
2020-03-03 13:02:38 -0800

    make xml stuff use xorstr

--- src/client/hooks.cpp
+++ src/client/hooks.cpp
@@ -18,126 +18,126 @@ void process_patch_command_node(
   using pugi::xml_object_range;
 
   for ( auto const &current : children ) {
-    if ( !_wcsicmp(current.name(), L"parent") ) {
+    if ( !_wcsicmp(current.name(), xorstr_(L"parent")) ) {
       process_patch_command_node(context.parent(), current.children());
     } else if ( auto context_attr = context.attribute() ) {
       auto context_node = context.parent();
-      if ( !_wcsicmp(current.name(), L"set-name") ) {
-        context_attr.set_name(current.attribute(L"name").value());
-      } else if ( !_wcsicmp(current.name(), L"set-value") ) {
-        context_attr.set_value(current.attribute(L"value").value());
-      } else if ( !_wcsicmp(current.name(), L"previous-attribute") ) {
+      if ( !_wcsicmp(current.name(), xorstr_(L"set-name")) ) {
+        context_attr.set_name(current.attribute(xorstr_(L"name")).value());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"set-value")) ) {
+        context_attr.set_value(current.attribute(xorstr_(L"value")).value());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"previous-attribute")) ) {
         process_patch_command_node({ context_attr.previous_attribute(), context.parent() }, current.children());
-      } else if ( !_wcsicmp(current.name(), L"next-attribute") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"next-attribute")) ) {
         process_patch_command_node({ context_attr.next_attribute(), context.parent() }, current.children());
-      } else if ( !_wcsicmp(current.name(), L"insert-attribute-before") ) {
-        process_patch_command_node({ context_node.insert_attribute_before(current.attribute(L"name").value(), context_attr), context_node }, current.children());
-      } else if ( !_wcsicmp(current.name(), L"insert-attribute-after") ) {
-        process_patch_command_node({ context_node.insert_attribute_after(current.attribute(L"name").value(), context_attr), context_node }, current.children());
-      } else if ( !_wcsicmp(current.name(), L"prepend-copy") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"insert-attribute-before")) ) {
+        process_patch_command_node({ context_node.insert_attribute_before(current.attribute(xorstr_(L"name")).value(), context_attr), context_node }, current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"insert-attribute-after")) ) {
+        process_patch_command_node({ context_node.insert_attribute_after(current.attribute(xorstr_(L"name")).value(), context_attr), context_node }, current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"prepend-copy")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"append-copy") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"append-copy")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"insert-copy-before") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"insert-copy-before")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"insert-copy-after") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"insert-copy-after")) ) {
         // not implemented
       }
     } else if ( auto context_node = context.node() ) {
-      if ( !_wcsicmp(current.name(), L"select-node") ) {
-        process_patch_command_node(context_node.select_node(current.attribute(L"query").value()), current.children());
-      } else if ( !_wcsicmp(current.name(), L"select-nodes") ) {
-        for ( auto &x : context_node.select_nodes(current.attribute(L"query").value()) ) {
+      if ( !_wcsicmp(current.name(), xorstr_(L"select-node")) ) {
+        process_patch_command_node(context_node.select_node(current.attribute(xorstr_(L"query")).value()), current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"select-nodes")) ) {
+        for ( auto &x : context_node.select_nodes(current.attribute(xorstr_(L"query")).value()) ) {
           process_patch_command_node(x, current.children());
         }
-      } else if ( !_wcsicmp(current.name(), L"prepend-attribute") ) {
-        process_patch_command_node({ context_node.prepend_attribute(current.attribute(L"name").value()), context_node }, current.children());
-      } else if ( !_wcsicmp(current.name(), L"append-attribute") ) {
-        process_patch_command_node({ context_node.append_attribute(current.attribute(L"name").value()), context_node }, current.children());
-      } else if ( !_wcsicmp(current.name(), L"prepend-child") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"prepend-attribute")) ) {
+        process_patch_command_node({ context_node.prepend_attribute(current.attribute(xorstr_(L"name")).value()), context_node }, current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"append-attribute")) ) {
+        process_patch_command_node({ context_node.append_attribute(current.attribute(xorstr_(L"name")).value()), context_node }, current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"prepend-child")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"append-child") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"append-child")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"prepend-children") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"prepend-children")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"append-children") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"append-children")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"prepend-copy") ) {
-        auto x = context_node.select_node(current.attribute(L"query").value());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"prepend-copy")) ) {
+        auto x = context_node.select_node(current.attribute(xorstr_(L"query")).value());
         if ( auto attr = x.attribute() ) {
           process_patch_command_node({ context_node.prepend_copy(attr), x.parent() }, current.children());
         } else if ( auto node = x.node() ) {
           process_patch_command_node(context_node.prepend_copy(node), current.children());
         }
-      } else if ( !_wcsicmp(current.name(), L"append-copy") ) {
-        auto x = context_node.select_node(current.attribute(L"query").value());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"append-copy")) ) {
+        auto x = context_node.select_node(current.attribute(xorstr_(L"query")).value());
         if ( auto attr = x.attribute() ) {
           process_patch_command_node({ context_node.append_copy(attr), x.parent() }, current.children());
         } else if ( auto node = x.node() ) {
           process_patch_command_node(context_node.append_copy(node), current.children());
         }
-      } else if ( !_wcsicmp(current.name(), L"prepend-move") ) {
-        if ( auto node = context_node.select_node(current.attribute(L"query").value()).node() ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"prepend-move")) ) {
+        if ( auto node = context_node.select_node(current.attribute(xorstr_(L"query")).value()).node() ) {
           process_patch_command_node(context_node.prepend_move(node), current.children());
         }
-      } else if ( !_wcsicmp(current.name(), L"append-move") ) {
-        if ( auto node = context_node.select_node(current.attribute(L"query").value()).node() ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"append-move")) ) {
+        if ( auto node = context_node.select_node(current.attribute(xorstr_(L"query")).value()).node() ) {
           process_patch_command_node(context_node.append_move(node), current.children());
         }
-      } else if ( !_wcsicmp(current.name(), L"attribute") ) {
-        process_patch_command_node({ context_node.attribute(current.attribute(L"name").value()), context_node }, current.children());
-      } else if ( !_wcsicmp(current.name(), L"attributes") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"attribute")) ) {
+        process_patch_command_node({ context_node.attribute(current.attribute(xorstr_(L"name")).value()), context_node }, current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"attributes")) ) {
         for ( auto &attr : context_node.attributes() ) {
           process_patch_command_node({ attr, context_node }, current.children());
         }
-      } else if ( !_wcsicmp(current.name(), L"child") ) {
-        process_patch_command_node(context_node.child(current.attribute(L"name").value()), current.children());
-      } else if ( !_wcsicmp(current.name(), L"children") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"child")) ) {
+        process_patch_command_node(context_node.child(current.attribute(xorstr_(L"name")).value()), current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"children")) ) {
         for ( auto &child : context_node.children() ) {
           process_patch_command_node(child, current.children());
         }
-      } else if ( !_wcsicmp(current.name(), L"find-child-by-attribute") ) {
-        process_patch_command_node(context_node.find_child_by_attribute(current.attribute(L"name").value(), current.attribute(L"attr-name").value(), current.attribute(L"attr-value").value()), current.children());
-      } else if ( !_wcsicmp(current.name(), L"first-attribute") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"find-child-by-attribute")) ) {
+        process_patch_command_node(context_node.find_child_by_attribute(current.attribute(xorstr_(L"name")).value(), current.attribute(xorstr_(L"attr-name")).value(), current.attribute(xorstr_(L"attr-value")).value()), current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"first-attribute")) ) {
         process_patch_command_node({ context_node.first_attribute(), context_node }, current.children());
-      } else if ( !_wcsicmp(current.name(), L"last-attribute") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"last-attribute")) ) {
         process_patch_command_node({ context_node.last_attribute(), context_node }, current.children());
-      } else if ( !_wcsicmp(current.name(), L"first-child") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"first-child")) ) {
         process_patch_command_node(context_node.first_child(), current.children());
-      } else if ( !_wcsicmp(current.name(), L"last-child") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"last-child")) ) {
         process_patch_command_node(context_node.last_child(), current.children());
-      } else if ( !_wcsicmp(current.name(), L"first-element-by-path") ) {
-        process_patch_command_node(context_node.first_element_by_path(current.attribute(L"path").value()), current.children());
-      } else if ( !_wcsicmp(current.name(), L"insert-child-after") ) {
-        if ( auto node = context_node.select_node(current.attribute(L"query").value()).node() )
-          process_patch_command_node(context_node.insert_child_after(current.attribute(L"name").value(), node), current.children());
-      } else if ( !_wcsicmp(current.name(), L"insert-child-before") ) {
-        if ( auto node = context_node.select_node(current.attribute(L"query").value()).node() )
-          process_patch_command_node(context_node.insert_child_before(current.attribute(L"name").value(), node), current.children());
-      } else if ( !_wcsicmp(current.name(), L"insert-copy-after") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"first-element-by-path")) ) {
+        process_patch_command_node(context_node.first_element_by_path(current.attribute(xorstr_(L"path")).value()), current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"insert-child-after")) ) {
+        if ( auto node = context_node.select_node(current.attribute(xorstr_(L"query")).value()).node() )
+          process_patch_command_node(context_node.insert_child_after(current.attribute(xorstr_(L"name")).value(), node), current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"insert-child-before")) ) {
+        if ( auto node = context_node.select_node(current.attribute(xorstr_(L"query")).value()).node() )
+          process_patch_command_node(context_node.insert_child_before(current.attribute(xorstr_(L"name")).value(), node), current.children());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"insert-copy-after")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"insert-copy-before") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"insert-copy-before")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"insert-move-after") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"insert-move-after")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"insert-move-before") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"insert-move-before")) ) {
         // not implemented
-      } else if ( !_wcsicmp(current.name(), L"next-sibling") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"next-sibling")) ) {
         process_patch_command_node(context_node.next_sibling(), current.children());
-      } else if ( !_wcsicmp(current.name(), L"remove-attribute") ) {
-        context_node.remove_attribute(current.attribute(L"name").value());
-      } else if ( !_wcsicmp(current.name(), L"remove-attributes") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"remove-attribute")) ) {
+        context_node.remove_attribute(current.attribute(xorstr_(L"name")).value());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"remove-attributes")) ) {
         context_node.remove_attributes();
-      } else if ( !_wcsicmp(current.name(), L"remove-child") ) {
-        context_node.remove_child(current.attribute(L"name").value());
-      } else if ( !_wcsicmp(current.name(), L"remove-children") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"remove-child")) ) {
+        context_node.remove_child(current.attribute(xorstr_(L"name")).value());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"remove-children")) ) {
         context_node.remove_children();
-      } else if ( !_wcsicmp(current.name(), L"root") ) {
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"root")) ) {
         process_patch_command_node(context_node.root(), current.children());
-      } else if ( !_wcsicmp(current.name(), L"set-name") ) {
-        context_node.set_name(current.attribute(L"value").value());
-      } else if ( !_wcsicmp(current.name(), L"set-value") ) {
-        context_node.set_value(current.attribute(L"value").value());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"set-name")) ) {
+        context_node.set_name(current.attribute(xorstr_(L"value")).value());
+      } else if ( !_wcsicmp(current.name(), xorstr_(L"set-value")) ) {
+        context_node.set_value(current.attribute(xorstr_(L"value")).value());
       }
     }
   }
@@ -148,7 +148,7 @@ static pugi::xml_document const s_xml_patches = []() {
   if ( SHGetKnownFolderPath(FOLDERID_Documents, KF_FLAG_DEFAULT, nullptr, &pszPath) == S_OK ) {
     fs::path path(pszPath);
     CoTaskMemFree(pszPath);
-    path /= L"BNS\\patches.xml";
+    path /= xorstr_(L"BnS\\patches.xml");
 
     pugi::xml_document doc;
     doc.load_file(path.c_str());
@@ -169,24 +169,24 @@ XmlDoc *Read_hook(
 
   if ( fileName && xmlDoc && xmlDoc->IsValid() ) {
     std::vector<pugi::xml_node> vec;
-    for ( auto const &x : s_xml_patches.select_nodes(L"/patches/patch") ) {
-      if ( FastWildCompare(x.node().attribute(L"filename").value(), fileName) ) {
+    for ( auto const &x : s_xml_patches.select_nodes(xorstr_(L"/patches/patch")) ) {
+      if ( FastWildCompare(x.node().attribute(xorstr_(L"filename")).value(), fileName) ) {
         vec.push_back(x.node());
       }
     }
     if ( !vec.empty() ) {
       pugi::xml_document doc;
       auto decl = doc.prepend_child(pugi::node_declaration);
-      decl.append_attribute(L"version") = L"1.0";
+      decl.append_attribute(xorstr_(L"version")) = xorstr_(L"1.0");
 
       int flags = pugi::format_raw;
       auto encoding = pugi::xml_encoding::encoding_auto;
-      if ( !_wcsicmp(fs::path(fileName).extension().c_str(), L".x16") ) {
-        decl.append_attribute(L"encoding") = L"utf-16";
+      if ( !_wcsicmp(fs::path(fileName).extension().c_str(), xorstr_(L".x16")) ) {
+        decl.append_attribute(xorstr_(L"encoding")) = xorstr_(L"utf-16");
         flags |= pugi::format_write_bom;
         encoding = pugi::xml_encoding::encoding_utf16;
       } else {
-        decl.append_attribute(L"encoding") = L"utf-8";
+        decl.append_attribute(xorstr_(L"encoding")) = xorstr_(L"utf-8");
       }
       if ( auto name = xmlDoc->Name() )
         doc.append_child(pugi::node_comment).set_value(name);
@@ -207,7 +207,7 @@ XmlDoc *Read_hook(
       std::array<WCHAR, MAX_PATH> PathName;
       std::array<WCHAR, MAX_PATH> TempFile;
       if ( GetTempPathW(SafeInt(PathName.size()), PathName.data())
-        && GetTempFileNameW(PathName.data(), L"bns", 0, TempFile.data())
+        && GetTempFileNameW(PathName.data(), xorstr_(L"bns"), 0, TempFile.data())
         && doc.save_file(TempFile.data(), L"", flags, encoding) ) {
 
         thisptr->Close(xmlDoc);
